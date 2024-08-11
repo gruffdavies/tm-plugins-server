19140e8b06fc5619dc1cea32857cc83be3ef6bba
Gruff Davies <gruff@gruffdavies.com>
2024-08-11 09:35:42 +0100

    Update rateLimiter.ts as it was too stingy

--- src/common/middleware/rateLimiter.ts
+++ src/common/middleware/rateLimiter.ts
@@ -6,12 +6,13 @@ import { logger } from '@/server';
 
 const rateLimiter = rateLimit({
   legacyHeaders: true,
-  limit: env.COMMON_RATE_LIMIT_MAX_REQUESTS ?? 20,
-  message: 'Too many requests, please try again later.',
+  limit: env.COMMON_RATE_LIMIT_MAX_REQUESTS ?? 1000, // Increased from 20 to 1000
+  message: 'Rate limit exceeded, please try again later.',
   standardHeaders: true,
-  windowMs: 15 * 60 * (env.COMMON_RATE_LIMIT_WINDOW_MS ?? 1000),
+  windowMs: 60 * 60 * 1000, // Set to 1 hour
   keyGenerator,
 });
+
 
 function keyGenerator(request: Request): string {
   if (!request.ip) {
